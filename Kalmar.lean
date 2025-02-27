60a1d8a49ad8f34224a9ccb02f6454b8480566d0
Rafael <rafaelaccetta@id.uff.br>
2025-02-27 16:13:26 -0300

    Create repo

---
+++ Kalmar.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Kalmar` library.
+-- Import modules here that should be built as part of the library.
+import Kalmar.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Kalmar
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
